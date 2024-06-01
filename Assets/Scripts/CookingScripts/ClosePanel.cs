117d2f4011cef519c3950b8cc4b457bb0c36fba0
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2024-06-01 17:23:21 -0300

    placing tools

---
+++ Assets/Scripts/CookingScripts/ClosePanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ClosePanel : MonoBehaviour, IPointerClickHandler {
+  public ToolPanel parent;
+
+  public void Start() {
+  }
+
+  public void OnPointerClick(PointerEventData eventData) {
+    gameObject.transform.parent.gameObject.SetActive(false);
+    Debug.Log("Clicked");
+  }
+}
--- Assets/Scripts/CookingScripts/DragAndDrop.cs
+++ Assets/Scripts/CookingScripts/DragAndDrop.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -8,14 +10,19 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
 {
     private RectTransform rect;
     private Image img;
+    public Image placingPosition;
+    private bool canPlace = false;
+    private Vector2 startPosition;
     
     // Start is called before the first frame update
     void Start() {
         rect = GetComponent<RectTransform>();
         img = GetComponent<Image>();
+        startPosition = transform.position;
     }
 
     public void OnDrag(PointerEventData eventData) {
+        placingPosition.gameObject.SetActive(true);
         rect.anchoredPosition += eventData.delta;
     }
 
@@ -25,5 +32,26 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
 
     public void OnEndDrag(PointerEventData eventData) {
         img.color = new Color(255, 255, 255, 255);
+        if (canPlace) {
+            rect.transform.position = placingPosition.transform.position;
+        } else {
+            rect.transform.position = startPosition;
+        }
+        placingPosition.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("trigger");
+        if (other.gameObject == placingPosition.gameObject) {
+            Debug.Log("Collided with position");
+            canPlace = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject == placingPosition.gameObject) {
+            Debug.Log("Collided with position");
+            canPlace = false;
+        }
     }
 }
