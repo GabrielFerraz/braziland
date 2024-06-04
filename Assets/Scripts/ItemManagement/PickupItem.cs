ab2455b7cacd2a0be96ba8568e715f851123b216
Fouad Elamoury <foad.osama@hotmail.com>
2024-06-04 15:40:24 +0300

    Item spawning and pickup item

--- Assets/Scripts/ItemManagement/PickupItem.cs
+++ Assets/Scripts/ItemManagement/PickupItem.cs
@@ -1,34 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PickupItem : MonoBehaviour
 {
-    Transform player;
+    Transform playerTransform;
+    Player player;
     [SerializeField] float speed = 5f;
     [SerializeField] float pickUpDistance = 1.5f;
     [SerializeField] float ttl = 10f;
+    public Item item;
+
+    public int count = 1;
     private void Awake()
     {
 
-        player = GameManager.instance.player.transform;
+        playerTransform = GameManager.instance.player.transform;
     }
     private void Update()
     {
         ttl -= Time.deltaTime;
         if (ttl < 0) { Destroy(gameObject); }
 
-        float distance = Vector3.Distance(transform.position, player.position);
+        float distance = Vector3.Distance(transform.position, playerTransform.position);
         if (distance > pickUpDistance)
             return;
 
         transform.position = Vector3.MoveTowards(
         transform.position,
-        player.position,
+        playerTransform.position,
         speed * Time.deltaTime
         );
 
         if (distance < 0.1f)
+        {
+            if(player.inventory != null)
+            {
+                player.inventory.Add(item);
+            }
+            else
+            {
+                Debug.Log("No inventory in Game Manager");
+            }
             Destroy(gameObject);
+        }
+    }
+    public void Set (Item item , int count)
+    {
+        this.item = item;
+        this.count = count;
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = item.data.icon;
     }
 }
